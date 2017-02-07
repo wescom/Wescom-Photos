7c4aadc7a82440161b739f505fdeb0ea288b1ff5
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-02-07 10:17:05 -0800

    cleaned up search bar, home page search, and added cart item to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
 
-
   def strip_subhead_tags(text)
     text.html_safe.gsub(/<p class="hl2_chapterhead">/, '<p>')
   end
 
+
+  def cart_quantity
+    @cart = Cart.find(session[:cart_id])
+    quantity = @cart.total_unique_items
+    return quantity.to_s
+  end
 end
