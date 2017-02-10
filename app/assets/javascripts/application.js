c26f2a2ec7ece977c085fa155f34ac318982cd19
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-02-09 16:02:22 -0800

    finetuned image gallery layout and modal images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,29 @@
 
 $(document).ready(function(){
 
-	// Image Gallery
+	//Disable cut copy paste on images
+    $('img').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+
+    //Disable mouse right click on images
+    $("img").on("contextmenu",function(e){
+        return false;
+    });
+
+	// Image Gallery - add class to image based on landscape or portrait
+	//	$('.modal_image2 img').each(function(){
+	//		alert((this.width) + " " +(this.height));
+	//	    $(this).addClass(this.width > this.height ? 'image_gallery_landscape' : 'image_gallery_portrait');
+	//	});
+
+	// Image Gallery Caption
 	$('.image_gallery_caption').hide();
-	$(".image_gallery_panel").mouseenter(function() {
-		$(this).find('.image_gallery_caption').fadeIn('fast');
-	});
-	$(".image_gallery_panel").mouseleave (function() {
-		$(this).find('.image_gallery_caption').fadeOut('fast')
-	});
+	//	$(".image_gallery_panel").mouseenter(function() {
+	//		$(this).find('.image_gallery_caption').fadeIn('fast');
+	//	});
+	//	$(".image_gallery_panel").mouseleave (function() {
+	//		$(this).find('.image_gallery_caption').fadeOut('fast')
+	//	});
 
 });
