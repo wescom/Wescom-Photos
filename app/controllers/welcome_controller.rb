f1117b508afce6b9ba9516b08f4f7829123ac355
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-02-15 08:40:47 -0800

    adding popular categories to home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @sports_image = StoryImage.joins(:story).where('subcategoryname = ?','High School').order('created_date DESC').first
+    @events_image = StoryImage.joins(:story).where('subcategoryname = ?','Community Sports').order('created_date DESC').first
+    @outdoors_image = StoryImage.joins(:story).where('categoryname = ?','Outdoors').order('created_date DESC').first
+    
   end
 end
