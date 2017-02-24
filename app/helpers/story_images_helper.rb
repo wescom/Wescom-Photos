88ccb9c53fd7d36c248f98511d9a75d291f39a35
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-02-24 14:50:15 -0800

    dont allow images already in cart to be added again. let user know image is already in cart

--- app/helpers/story_images_helper.rb
+++ app/helpers/story_images_helper.rb
@@ -1,2 +1,16 @@
 module StoryImagesHelper
+  
+  def image_in_cart?(image_id)
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+      @cart_item = @cart.cart_items.where(:item_id => image_id)
+      if @cart_item.count > 0
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
 end
