8539b02dc26fdc8822986c32302975de693463ae
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-03-09 12:08:17 -0800

    copy local database.yml up to production server during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,6 +65,7 @@ namespace :deploy do
         execute "mkdir -p #{shared_path}/config"
         upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
         upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/application.yml"
+        upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
       end
     end
   end
