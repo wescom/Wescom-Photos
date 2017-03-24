cd51e10944e6c818c9dadd4f4b5af3fce1a5db0a
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-03-24 13:02:41 -0700

    changes home image search order

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,7 +95,7 @@ namespace :deploy do
       execute "rm -rf #{release_path}/solr #{release_path}/log #{release_path}/public/system #{release_path}/tmp/pids"
       execute "ln -s /WescomArchive/solr #{release_path}/solr"
       execute "mkdir -p #{release_path}/public && ln -s #{shared_path}/system #{release_path}/public/system"
-      execute "ln -s /WescomArchive/db_images #{shared_path}/system/db_images && ln -s /WescomArchive/pdf_images #{shared_path}/system/pdf_images"
+      #execute "mkdir -p #{shared_path}/system && ln -s /WescomArchive/db_images #{shared_path}/system/db_images && ln -s /WescomArchive/pdf_images #{shared_path}/system/pdf_images"
       execute "ln -s #{shared_path}/log #{release_path}/log"
     end
   end
