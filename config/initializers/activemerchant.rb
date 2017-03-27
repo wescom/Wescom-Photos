5a5ad1a4ab4f64183b9e321297884a11791b76a2
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-03-27 11:12:12 -0700

    trying to get activemerchant working

--- config/initializers/activemerchant.rb
+++ config/initializers/activemerchant.rb
@@ -6,8 +6,14 @@ if Rails.env == "development"
   login = "JF4279-06"
   password="6I3dPIjyqGoD3WAUDGONJgq8sEbKp7TI"
 elsif Rails.env == "production"
-  login = 'JF4279-06'
-  password='Go2yskAM!'
+#  login = 'JF4279-06'
+#  password='Go2yskAM!'
+  ActiveMerchant::Billing::FirstdataE4Gateway.wiredump_device = File.open(Rails.root.join("log","active_merchant.log"), "a+")
+  ActiveMerchant::Billing::FirstdataE4Gateway.wiredump_device.sync = true
+  ActiveMerchant::Billing::Base.mode = :test
+
+  login = "JF4279-06"
+  password="6I3dPIjyqGoD3WAUDGONJgq8sEbKp7TI"
 end
 GATEWAY = ActiveMerchant::Billing::FirstdataE4Gateway.new({
       login: login,
