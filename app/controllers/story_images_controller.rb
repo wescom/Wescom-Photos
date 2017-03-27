7edb62a9d0c9be1830155928185db55d5b0873cb
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-03-27 15:56:08 -0700

    adjust story_image search to handle nil default search settings

--- app/controllers/story_images_controller.rb
+++ app/controllers/story_images_controller.rb
@@ -7,15 +7,19 @@ class StoryImagesController < ApplicationController
       begin
         @story_images = StoryImage.search(:include => [:story]) do
           all do
-            fulltext params[:search_query]
-            any do  # Filter all searches by caption text, set within default_settings
-              fulltext default_settings.search_for_caption_text, :fields => :media_webcaption
-              fulltext default_settings.search_for_caption_text, :fields => :media_printcaption
-              fulltext default_settings.search_for_caption_text, :fields => :media_originalcaption
+            fulltext params[:search_query], :fields => [:media_webcaption, :media_printcaption, :media_originalcaption, :story_category_name, :story_subcategory_name]
+            any do  
+              # Filter all searches by caption text set within default_settings, ie. contains 'Bulletin'
+              fulltext default_settings.search_for_caption_text, :fields => [:media_webcaption, :media_printcaption, :media_originalcaption]
             end
-            any_of do  # Filter all searches by publish status and priority, set within default_settings
-              with(:publish_status, default_settings.search_for_publish_status)
-              with(:priority, default_settings.search_for_priority)
+            any_of do  
+              # Filter all searches by publish status and priority set within default_settings, ie. contains 'Published' and 'Web Ready'
+              if !default_settings.search_for_publish_status.empty?
+                with(:publish_status, default_settings.search_for_publish_status)
+              end
+              if !default_settings.search_for_priority.empty?
+                with(:priority, default_settings.search_for_priority)
+              end
             end
           end
           paginate(:page => params[:page], :per_page => 24)
