2ed229cd916fa9320c0e2a6c1bb8cce130c3fe18
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-03-27 16:40:43 -0700

    fixed home page to only use images that meet the same default search setting requirements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,20 @@ class HomeController < ApplicationController
     if DefaultSetting.exists? 
       @default_settings = DefaultSetting.first
     
-      @cat1_image = StoryImage.joins(:story).published.where('categoryname = ? OR subcategoryname = ?',@default_settings.home_image_cat1, @default_settings.home_image_cat1).order_by_pubdate.first
-      @cat2_image = StoryImage.joins(:story).published.where('categoryname = ? OR subcategoryname = ?',@default_settings.home_image_cat2, @default_settings.home_image_cat2).order_by_pubdate.first
-      @cat3_image = StoryImage.joins(:story).published.where('categoryname = ? OR subcategoryname = ?',@default_settings.home_image_cat3, @default_settings.home_image_cat3).order_by_pubdate.first
+      @cat1_image = StoryImage.joins(:story)
+      @cat1_image = @cat1_image.published.where('categoryname = ? OR subcategoryname = ?',@default_settings.home_image_cat1, @default_settings.home_image_cat1)
+      @cat1_image = @cat1_image.where('media_webcaption like ?',"%#{@default_settings.search_for_caption_text}%")
+      @cat1_image = @cat1_image.order_by_pubdate.first
+
+      @cat2_image = StoryImage.joins(:story)
+      @cat2_image = @cat2_image.published.where('categoryname = ? OR subcategoryname = ?',@default_settings.home_image_cat2, @default_settings.home_image_cat2)
+      @cat2_image = @cat2_image.where('media_webcaption like ?',"%#{@default_settings.search_for_caption_text}%")
+      @cat2_image = @cat2_image.order_by_pubdate.first
+
+      @cat3_image = StoryImage.joins(:story)
+      @cat3_image = @cat3_image.published.where('categoryname = ? OR subcategoryname = ?',@default_settings.home_image_cat3, @default_settings.home_image_cat3)
+      @cat3_image = @cat3_image.where('media_webcaption like ?',"%#{@default_settings.search_for_caption_text}%")
+      @cat3_image = @cat3_image.order_by_pubdate.first
 
     else  # If no default settings record, then create one and send user to edit
       @default_setting = DefaultSetting.new
