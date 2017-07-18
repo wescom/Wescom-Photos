5de8f871f02f1755c6d4a487e9736ed9285088dc
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-07-18 16:08:54 -0700

    filter cc parameter from logs

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module WescomPhotos
       g.orm :active_record, primary_key_type: :uuid
     end
 
+    config.filter_parameters += [:password, :password_confirmation, :credit_card, :credit_card_number]
   end
 end
