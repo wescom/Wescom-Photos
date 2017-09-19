4b11c7af495ed3a6f8b6c2eb5ad90774a330efe5
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-09-19 16:25:53 -0700

    adjust pub dropdown select to only show pubs with at least one pdf_image

--- app/controllers/pdf_images_controller.rb
+++ app/controllers/pdf_images_controller.rb
@@ -6,7 +6,8 @@ class PdfImagesController < ApplicationController
 
     @plans = Plan.where("location_id" => 1)
     @plans = @plans.where('publication_type_id IN (?)', default_settings.search_for_pdf_pubtypeId.split(",").map(&:to_i))
-    @publications = @plans.select(:pub_name).uniq.order(:pub_name)
+    @publications = @plans.select(:pub_name).joins(:pdf_images).uniq.order(:pub_name)
+puts "********"+@publications.inspect
     @section_letters = @plans.joins(:pdf_images).select("pdf_images.section_letter").uniq.order("pdf_images.section_letter")
 
     if params[:search_query]
