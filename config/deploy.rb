1601d327c078d14e435a0cba33da5464ce0ff02a
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-12-07 07:59:07 -0800

    increased puma workers to 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,8 @@ set :application, "wescomphotos"
 set :scm_username,    "wescomarchive"     # Git user
 set :scm_passphrase,  "Go2cmdarchive"     # Git password
 
-set :puma_threads,    [4, 16]
-set :puma_workers,    2
+#set :puma_threads,    [4, 16]
+#set :puma_workers,    2
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -52,9 +52,9 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
+#set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+#set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
