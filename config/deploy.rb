39c8deeb81915b43a82d43fad29e92c620d2216c
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-12-07 08:20:10 -0800

    increased puma workers to 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,9 +52,9 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-#set :puma_preload_app, true
-#set :puma_worker_timeout, nil
-#set :puma_init_active_record, false  # Change to true if using ActiveRecord
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
