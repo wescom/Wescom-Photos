ee8d11de2e313ef2975ae42d713ded36e858e955
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-12-07 08:29:12 -0800

    increased puma workers to 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :scm_username,    "wescomarchive"     # Git user
 set :scm_passphrase,  "Go2cmdarchive"     # Git password
 
 set :puma_threads,    [4, 16]
-set :puma_workers,    2
+set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
