31b86122bc31c30a25cb81261668ed4a6eb5d4ea
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-01-11 16:19:04 -0800

    fixing bug with story_id and media_id parameters

--- app/controllers/story_images_controller.rb
+++ app/controllers/story_images_controller.rb
@@ -46,7 +46,7 @@ class StoryImagesController < ApplicationController
 Rails.logger.info "params.has_key?(:media_id) && params.has_key?(:story_id)"
       if !params[:story_id].empty? && !params[:media_id].empty?
 Rails.logger.info "!params[:story_id].empty? && !params[:media_id].empty?"
-        @story = Story.find(params[:story_id])
+        @story = Story.find_by_id(params[:story_id])
 Rails.logger.info "@story "+@story.inspect
         if !@story.nil?
 Rails.logger.info "@story not nil"
