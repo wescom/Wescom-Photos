e519343809c370414ceae87f2cef5784d4115958
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-01-18 08:15:00 -0800

    cleaned up styling in confirmation emails. also added extension to file downloads.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -111,11 +111,11 @@ class OrdersController < ApplicationController
 
     if params[:item_type] == "StoryImage"
       @image = StoryImage.find(@order_item.item_id)
-      send_file @image.image.path, :filename => "image_"+@image.id.to_s
+      send_file @image.image.path, :filename => "image_"+@image.id.to_s+".jpg"
       Rails.logger.info "***** Image Downloaded ***** " + @image.image.path
     else
       @pdf = PdfImage.find(@order_item.item_id)
-      send_file @pdf.image.path, :filename => "image_"+@pdf.id.to_s
+      send_file @pdf.image.path, :filename => "image_"+@pdf.id.to_s+".pdf"
       Rails.logger.info "***** News Page Downloaded ***** " + @pdf.image.path
     end
   end
