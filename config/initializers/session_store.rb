aafaed54a3c7c0c45f18364776d9918594f434e6
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-02-03 20:04:18 -0800

    trying to fix CSRF token auth error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: 'Wescom-Photos_#{Rails.env}'
+Rails.application.config.session_store :cookie_store, key: '_Wescom-Photos_session', secure: Rails.env.production?
