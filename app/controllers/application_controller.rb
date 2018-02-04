d154f79af171940c8fb52e666ba2550a81418130
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-02-03 19:31:30 -0800

    trying to fix CSRF token auth error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-#  protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
-  
+  protect_from_forgery with: :exception
     include ApplicationHelper
     helper_method :current_user
 
