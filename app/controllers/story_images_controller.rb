3fd003fa476ffe0a4da6608ad1d1a9f9e690b8fe
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-02-09 14:35:33 -0800

    fixed story_id bug. use story.doc_id to find images by story_id NOT id

--- app/controllers/story_images_controller.rb
+++ app/controllers/story_images_controller.rb
@@ -57,8 +57,10 @@ class StoryImagesController < ApplicationController
     if params.has_key?(:media_id) && params.has_key?(:story_id)
       if !params[:story_id].empty? && !params[:media_id].empty?
         @story = Story.find_by_doc_id(params[:story_id])
+        Rails.logger.info @story.inspect
         if !@story.nil?
           @story_image = @story.story_images.find_by_media_id(params[:media_id]) unless @story.nil?
+          Rails.logger.info @ststory_imageory.inspect
           params[:id] = @story_image.id unless @story_image.nil?
         end
       end
